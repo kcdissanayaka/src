46c1c291fe433d8c16a2fae360a801b330674327
kcdissanayaka <kc.dissanayaka@outlook.com>
2024-01-29 13:51:33 +0200

    CreateUniversalHeaderAndFooterToLankanMatkaSite

--- Kasun_Tasks/ex1.php
+++ Kasun_Tasks/ex1.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Exercise 1: Getting Started with PHP - Kasun </title>
 </head>
 <body>
     <h3>3.1 Write PHP code to output the following message:
---
+++ hello.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHPLankan Matka </title>
+</head>
+<body>
+    <?php
+        //This is a comment
+        echo" <h1> Headder </h1>"; 
+        echo "<p>Hello PHP my name is Kasun </P>";
+        $name ="Thinethi Dissanayaka";
+
+        echo "<p>My Daughter's Name is .$name.</p>  ";
+    ?>
+</body>
+</html>
+
