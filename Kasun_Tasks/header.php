0adb75d82df2a1c3a77098d1afd09a9398c17ffb
kcdissanayaka <kc.dissanayaka@outlook.com>
2024-01-30 14:50:59 +0200

    ChangetheStructureOftheSubmissionasPerTheInstructionEmailSent

--- Kasun_Tasks/header.php
+++ Kasun_Tasks/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Lankan Matka</title>
+    <title><?php echo $title ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
@@ -23,24 +23,24 @@
                         <ul class="sidebar">
                         <li onclick=hideSidebar()><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 -960 960 960" width="26"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
                         <li><a href="ex1.php">Exercise1</a></li>
-                        <li><a href="#home">Home</a></li>
-                        <li><a href="#booking">Book</a></li>
+                        <li><a href="ex2.php">Exercise2</a></li>
+                        <!--<li><a href="#booking">Book</a></li>
                         <li><a href="#plan">Packages</a></li>
                         <li><a href="#services">Services</a></li>
                         <li><a href="#gallery">Gallery</a></li>
                         <li><a href="#footer">Contact Us</a></li>
-                        <li><a href=""><i class="fa fa-sign-in" aria-hidden="true"></i>Sign In</a></li>
+                        <li><a href=""><i class="fa fa-sign-in" aria-hidden="true"></i>Sign In</a></li> -->
                         </ul>
                         <ul>
                         <li><a href="ex1.php">Exercise1</a></li>   
-                        <li class="hideOnMobile"><a href="#home">Home</a></li>
-                        <li class="hideOnMobile"><a href="#booking">Book</a></li>
+                        <li class="hideOnMobile"><a href="ex2.php">Exercise2</a></li>
+                        <!-- <li class="hideOnMobile"><a href="#booking">Book</a></li>
                         <li class="hideOnMobile"><a href="#plan">Packages</a></li>
                         <li class="hideOnMobile"><a href="#services">Services</a></li>
                         <li class="hideOnMobile"><a href="#gallery">Gallery</a></li>
                         <li class="hideOnMobile"><a href="#footer">Contact Us</a></li>
                         <li class="hideOnMobile"><a href=""><i class="fa fa-sign-in" aria-hidden="true"></i>Sign In</a></li>
-                        <li onclick="showSidebar()" class="menu-button"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 -960 960 960" width="26"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
+                        <li onclick="showSidebar()" class="menu-button"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 -960 960 960" width="26"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li> -->
                         </ul>
                     </nav>
 
@@ -63,15 +63,15 @@
             <div class="section text-center">
                 <div class="row">
                 <div class="textOnVideo col">
-                        <h1>ADVENTURE AWAITS YOU</h1>
+                        <h1>WEB PROGRAMMING DEVELOPMENT</h1>
                         <h3>Explore new places with us, with lifelong memories</h3>
                     </div>
                     <div class="video-container col embed-responsive embed-responsive-16by9">
                         <video autoplay muted loop class="video-bg embed-responsive-item">
-                            <source src="assets/video/ambuluwawa.mp4">
+                            <!--<source src="assets/video/ambuluwawa.mp4">-->
                         </video>
                         <a href="#booking" class="btn btn-warning btn-header-book mx-auto">Book Now</a>
                     </div>
                 </div>
-            </div>
+            </div> 
         </section>
