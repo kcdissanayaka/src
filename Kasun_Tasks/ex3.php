4d1a6ccb4e394078a50a377f7e05a681166232eb
kcdissanayaka <kc.dissanayaka@outlook.com>
2024-02-02 10:39:00 +0200

    CreateGroupGitRepo

--- Kasun_Tasks/ex3.php
+++ Kasun_Tasks/ex3.php
@@ -10,26 +10,27 @@ include 'header.php'; ?>
                     </div>
                 </div>
                 <div class="container text-left">
-                    <div class="row tourPlanCards">
+                    <div class="row">
                         <div class="col-md-12">
-                         <h3>1. Create a file (ex3.php): Create a file named ex3.php in your exercise folder. Ensure that it uses the same layout as your other pages and that your navigation bar includes a link to this exercise.</h3>
-                         <h3>2. Form Creation: Create a simple HTML form to collect the user’s Firstname and Lastname. Use the echo statement to print “Hello [Firstname] [Lastname], You are welcome to my site.” inside an <h3> tag. </h3>
-                         <form>
-                             
-                               <div class="form-group"> 
-                                <label for="First Name:">First Name </label> 
-                                <input type="text" class= "form-control" name="Firstname" required placeholder="Enter First Name"><br><br>
-                               <lable for= "Last Name:"> Last Name:</label>
-                                <input type="text" class= "form-control" name="Lastname" requirerd placeholder="Enter Last Name"><br><br>
-                                <input type="Submit" value="Submit">
+                            <h3>1. Create a file (ex3.php): Create a file named ex3.php in your exercise folder. Ensure that it uses the same layout as your other pages and that your navigation bar includes a link to this exercise.</h3>
+                            <h3>2. Form Creation: Create a simple HTML form to collect the user’s Firstname and Lastname. Use the echo statement to print “Hello [Firstname] [Lastname], You are welcome to my site.” inside an <h3> tag. </h3> <br>
+                            <form action="" method="get">
+                                <div class="col-md-4"> 
+                                    <label for="First Name:">First Name: </label> 
+                                    <input type="text" class= "form-control" name="Firstname" required placeholder="Enter First Name"><br>
+                                    <lable for= "Last Name:"> Last Name:</label>
+                                    <input type="text" class= "form-control" name="Lastname" requirerd placeholder="Enter Last Name"><br>
+                                    <input type="Submit button" class= "btn btn-warning" value="Submit">
+                                
                                 </div> 
-                         </form>
-                        
-                         <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, optio a reprehenderit perferendis quo rerum officiis ipsam laborum ad necessitatibus totam cum, nihil dignissimos? 
-                            Facere ipsam ipsa omnis sapiente nisi?</p>
 
-                        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, optio a reprehenderit perferendis quo rerum officiis ipsam laborum ad necessitatibus totam cum, nihil dignissimos? 
-                        Facere ipsam ipsa omnis sapiente nisi?</p>
+                                </div>
+                                
+                            </form> <br>
+                        </div>
+                        
+                        $Firstname =
+                         
 
     </div>
     </div>
